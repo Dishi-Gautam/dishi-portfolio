7041e6cd87f2fa7378bb24b0c755d371e771a866
dishiiiii <139355782+Dishi-Gautam@users.noreply.github.com>
2025-12-14 17:27:42 +0530

    Delete src directory

--- src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="footer">
-      <div className="footer-inner">
-        <small>© {new Date().getFullYear()} Dishi Gautam</small>
-      </div>
-    </footer>
-  )
-}
